5fe8f712b3dd0e07e94b7f6d8b9fd587ad748cc8
Наталья Романова <Natalocka_ne@mail.ru>
2024-01-13 15:25:14 +0300

    commit before pull

--- src/app.js
+++ src/app.js
@@ -1,12 +1,46 @@
 import './styles.css'
 import {ContextMenu} from "./menu.js"
 
+import {DrawingModule} from "./modules/drawing.module.js"
+import {BackgroundModule} from "./modules/background.module.js"
+// import {RandomMessage} from "./modules/message.module.js"
 
-let menu = new ContextMenu()
+function addMenuEvent() {
+    const ul = document.querySelector("#menu")
+    ul.addEventListener("click", event => {
+        const {target} = event
+        if (target.dataset.type === "BackgroundModule") {
+            document.body.style.background = "green"
+            backgroundModule.trigger()
+        }
+        else if (target.dataset.type === "ClicksModule") {
+            document.body.style.background = "red"
+            clicksModule.trigger()
+        }
+        // else if (target.dataset.type === "RandomMessage") {
+        //     document.body.style.background = "black"
+        //     randomMessage.trigger()
+        // }
+        else if (target.dataset.type === "DrawingModule") {
+            drawingModule.trigger()
+        }
+
+    })
+}
+
+const backgroundModule = new BackgroundModule("BackgroundModule","Поменять цвет")
+const drawingModule = new DrawingModule("DrawingModule", "Рисовалка")
+// const randomMessage = new RandomMessage("RandomMessage", "Вызвать сообщение")
+
+const menu = new ContextMenu()
+
+menu.add(backgroundModule)
+menu.add(drawingModule)
+// menu.add(randomMessage)
+
 menu.open()
 menu.close()
 
 
 
-
-
+addMenuEvent()
