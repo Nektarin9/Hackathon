86cdf2e8bb80e088ed8e7c52b6c5f529e38bdd62
Kulibars <125401672+Kulibars@users.noreply.github.com>
2024-01-13 14:26:08 +0300

    BackgroundModule

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -2,6 +2,11 @@ import {Module} from '../core/module'
 
 export class BackgroundModule extends Module {
     changeBackground() {
-        
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
     }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,45 @@
+import {ShapeModule} from './modules/shape.module'
+
 export function random(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1))
+}
+
+export function getOneFigure() {
+  figure = new ShapeModule();
+  const figureOne = document.createElement('div');
+  figureOne.classList.add('figure');
+
+  figureOne.style.position = 'absolute'
+  figureOne.style.width = `${figure.getRandomSize(10, 200)}px`
+  figureOne.style.height = `${figure.getRandomSize(10, 200)}px`
+  figureOne.style.borderRadius = `${figure.getRandomSize(0, 50)}px`
+  figureOne.style.top = `${figure.getRandomSize(10, 90)}%`
+  figureOne.style.left = `${figure.getRandomSize(10, 90)}%`
+  figureOne.style.backgroundColor = `${figure.changeBackground()}`
+  figureOne.style.transform = `rotate(${figure.randomNum(360)}deg)`
+  document.body.append(figureOne);
+}
+
+export function getTwoFigure() {
+  figure = new ShapeModule();
+  const figureTwo = document.createElement('div');
+  figureTwo.style.position = 'absolute'
+  figureTwo.style.width = `${figure.getRandomSize(10, 200)}px`
+  figureTwo.classList.add('figure');
+  figureTwo.style.top = `${figure.getRandomSize(10, 90)}%`
+  figureTwo.style.left = `${figure.getRandomSize(10, 90)}%`
+  figureTwo.style.border = `${figure.getRandomSize(10, 90)}px solid transparent`
+  let randomBorder = figure.randomNum(4)
+  if (randomBorder === 1) {
+      figureTwo.style.borderTop = `${figure.getRandomSize(10, 90)}px solid ${figure.changeBackground()}`
+  } else if (randomBorder === 2) {
+      figureTwo.style.borderBottom = `${figure.getRandomSize(10, 90)}px solid ${figure.changeBackground()}`
+  } else if (randomBorder === 3) {
+      figureTwo.style.borderLeft = `${figure.getRandomSize(10, 90)}px solid ${figure.changeBackground()}`
+  } else if (randomBorder === 4) {
+      figureTwo.style.borderRight = `${figure.getRandomSize(10, 90)}px solid ${figure.changeBackground()}`
+  }
+  figureTwo.style.transform = `rotate(${figure.randomNum(360)}deg)`
+  
+  document.body.append(figureTwo);
 }
