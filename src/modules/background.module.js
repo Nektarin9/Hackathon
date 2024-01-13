c41a3555627fb585e85e90ec62a6e36fb9de20a6
Kulibars <125401672+Kulibars@users.noreply.github.com>
2024-01-13 15:14:20 +0300

    get randon ShapeModule

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,17 +1,12 @@
 import {Module} from '../core/module'
 
 export class BackgroundModule extends Module {
-<<<<<<< HEAD
-    changeBackground() {
+    trigger() {
         const letters = '0123456789ABCDEF';
         let color = '#';
         for (let i = 0; i < 6; i++) {
           color += letters[Math.floor(Math.random() * 16)];
         }
-        return color;
-=======
-    trigger() {
-        console.log(1)
->>>>>>> 00fcb11c15f7ad29ade432e31f49b033956db99e
+        document.body.style.backgroundColor = color
     }
 }
