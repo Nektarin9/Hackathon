039de2049396dde965cff30158e30df27f80b791
Nektarin <vlad-kasparov@mail.ru>
2024-01-13 21:14:18 +0300

    test

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import {ClicksModule} from "./modules/clicks.module.js"
 import {BackgroundModule} from "./modules/background.module.js"
 import {RandomMessage} from "./modules/message.module.js"
 import {ShapeModule} from "./modules/shape.module.js"
-import { DrawingModule } from "./modules/drawing.module";
+import {DrawingModule} from "./modules/drawing.module";
+import {RndAudioModule} from "./modules/randomAudio.module.js"
+
+// туу тесты
+
+const myAudio1 = './src/test/18775-rickroll354.mp3'
+const myAudio2 = './src/test/716328-5435434.mp3'
+const myAudio3 = './src/test/7163335354.mp3'
+const myAudio4 = './src/test/71633153454.mp3'
+const myAudio5 = './src/test/716334423432.mp3'
+
+const audios = [myAudio1, myAudio2, myAudio3, myAudio4, myAudio5];
+
+let randomNum = () => { return Math.ceil(audios.length * Math.random()) }
+
+console.log(randomNum())
+
+const audio = new Audio();
+function soundClick() {
+    audio.src = audios[randomNum() - 1];
+    audio.autoplay =true;
+}
+let btn = document.querySelector("button")
+
+btn.addEventListener("click", event => {
+    soundClick();
+})
+
+// ..................
 
 function addMenuEvent() {
     const ul = document.querySelector("#menu")
@@ -26,6 +54,8 @@ function addMenuEvent() {
         else if (target.dataset.type === "DrawingModule") {
             drawingModule.trigger()
         }
+        else if (target.dataset.type === "RndAudioModule") {
+        }
     })
 
 }
@@ -35,15 +65,23 @@ const clicksModule = new ClicksModule("ClicksModule", "Считать кнопк
 const randomMessage = new RandomMessage("RandomMessage", "Вызвать сообщение")
 const shapeModule = new ShapeModule("ShapeModule", "Случайная фигура")
 const drawingModule = new DrawingModule("DrawingModule", "Рисовалка")
+const rndAudioModule = new RndAudioModule("RndAudioModule", "Случайный звук")
+
 
 const menu = new ContextMenu()
+
+
+
+
+
+
 
 menu.add(backgroundModule)
 menu.add(clicksModule)
 menu.add(randomMessage)
 menu.add(shapeModule)
 menu.add(drawingModule)
-
+menu.add(rndAudioModule)
 
 menu.open()
 menu.close()
---
+++ src/modules/randomAudio.module.js
@@ -0,0 +1,9 @@
+import {Module} from '../core/module'
+
+
+export class RndAudioModule extends Module {
+    trigger() {
+       
+       
+    }
+}
